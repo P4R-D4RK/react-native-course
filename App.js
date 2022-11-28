7d4d1dd06c99acd0a4c390eba7c5feaf4b83e3fe
Luis Polina <luis.282001@gmail.com>
2022-11-28 17:40:48 -0600

    image and buttons

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World!</Text>
+      <Image
+        source={{ uri: "https://picsum.photos/200/200" }}
+        style={styles.image}
+      ></Image>
+      <TouchableOpacity
+        onPress={() => Alert.alert("Hello")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -17,6 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#292929",
   },
   title: { fontSize: 30, color: "white" },
+  image: { height: 200, width: 200, borderRadius: 100 },
+  button: { backgroundColor: "deepskyblue", padding: 7, marginTop: 10 },
+  buttonText: { color: "white", fontSize: 20 },
 });
 
 export default App;
